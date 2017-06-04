3caad87468e3984c6e9dbd31b1c64525c97d9359
Märt Konsap <mrt.konsap@gmail.com>
2017-06-04 21:35:14 +0300

    Created example of config file and removed config

--- src/main/java/bot/Config.java
+++
@@ -1,8 +0,0 @@
-package bot;
-
-class Config {
-    private Config() {}
-
-    static final String JAVABOT_USER = "<NAME>";
-    static final String JAVABOT_TOKEN = "<TOKEN>";
-}
---
+++ src/main/java/bot/ConfigExample.java
@@ -0,0 +1,8 @@
+package bot;
+
+class ConfigExample { //todo rename to 'Config'
+    private ConfigExample() {}
+
+    static final String JAVABOT_USER = "<CHANGE_ME>";
+    static final String JAVABOT_TOKEN = "<CHANGE_ME>";
+}
